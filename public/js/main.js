e20fd5bebda3937d193f03688f39f868ca655c6b
kubiceq <34571984+kubiceq@users.noreply.github.com>
2021-02-09 21:26:20 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
-const socket = io();
+const socket = io('https://wyrkubqo.herokuapp.com/');
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
